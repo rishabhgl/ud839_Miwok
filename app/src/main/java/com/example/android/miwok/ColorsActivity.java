a19b7d6247a0a3288358dc71b46a72c2b9bbd4d4
rishabhgl <rgoyal2_be21@thapar.edu>
2022-08-02 18:37:07 +0530

    All lists updated

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -3,6 +3,9 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_layout);
+
+        ListView colorsList = findViewById(R.id.list);
+
+        ArrayList<Word> colors = new ArrayList<Word>(8);
+        colors.add(new Word("red","wetetti"));
+        colors.add(new Word("green","chokokki"));
+        colors.add(new Word("brown","takaakki"));
+        colors.add(new Word("grey","topoppi"));
+        colors.add(new Word("black","tululli"));
+        colors.add(new Word("white","kelelli"));
+        colors.add(new Word("dusty yellow","ṭopiisә"));
+        colors.add(new Word("mustard yellow","chiwiiṭә"));
+
+        WordArrayAdapter colorsAdapter = new WordArrayAdapter(this,colors);
+        colorsList.setAdapter(colorsAdapter);
     }
+
+
 }
--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -3,6 +3,9 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_layout);
+
+        ArrayList<Word> familyMembers = new ArrayList<Word>();
+        familyMembers.add(new Word("father","әpә"));
+        familyMembers.add(new Word("mother","әṭa"));
+        familyMembers.add(new Word("son","angsi"));
+        familyMembers.add(new Word("daughter","tune"));
+        familyMembers.add(new Word("older brother","taachi"));
+        familyMembers.add(new Word("younger brother","chalitti"));
+        familyMembers.add(new Word("older sister","teṭe"));
+        familyMembers.add(new Word("younger sister","kolliti"));
+        familyMembers.add(new Word("grandmother","ama"));
+        familyMembers.add(new Word("grandfather","paapa"));
+
+        WordArrayAdapter itemsAdapter = new WordArrayAdapter(this,familyMembers);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+
+
     }
 }
